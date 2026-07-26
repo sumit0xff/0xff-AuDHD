c5515161f35a5ed13166938fd0193bcc58c3d76a
SUMIT <216392422+sumit0xff@users.noreply.github.com>
2026-07-26 20:28:50 +0530

    chore: remove generated files from repository

--- dist/shared/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DEFAULT_CONFIG_FILE = exports.APP_NAME = void 0;
-exports.APP_NAME = '0xff';
-exports.DEFAULT_CONFIG_FILE = '.0xff/config.json';
